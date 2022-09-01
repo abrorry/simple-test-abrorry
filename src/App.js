e9c0a8030309ee866641c1d551de0eed22046bc4
abrorry <abrorry@gmail.com>
2022-09-01 16:27:40 +0700

    Jawban achmad riesky abrorry

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import "./styles.css";
+import React, { useState } from 'react';
+import './styles.css';
 
 export default function App() {
-  function handleInput() {
-    //please put your logic here
-  }
+  const [input, setInput] = useState('');
+  const handleInput = () => {
+    const arr = input.split('');
+    arr.reverse();
+    const reverse = Number(arr.join(''));
+    return {
+      reverse,
+      diff: Math.abs(Number(input) - Number(reverse)),
+    };
+  };
 
   return (
-    <div className="App">
+    <div className='App'>
       <div>
-        Number: <input />
-        <button>Submit</button>
+        <div>
+          Number:{' '}
+          <input
+            type='number'
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
+          <button type='submit'>Submit</button>
+        </div>
+        <div>{`Result:${handleInput().reverse}`}</div>
+        <div>{`Differences:${handleInput().diff}`}</div>
       </div>
-      <div>Result: 0</div>
     </div>
   );
 }
